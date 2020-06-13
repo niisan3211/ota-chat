0c16e161e9158849f170f5feeb7268a2b95a519f
niisan3211 <kappahaookikunaritai@icloud.com>
2020-06-13 22:38:33 +0900

    add:アソシエーション
    アソシエーションの追加

--- app/models/genru.rb
+++ app/models/genru.rb
@@ -2,6 +2,9 @@ class Genru < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   has_many :users, through: :users_genrus
+  has_many :tweets
+  has_many :groups
+
 
   def self.search(search)
     if search
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,5 @@
 class Group < ApplicationRecord
+  belogns_to :genru
+  has_many :tweets
+  has_many :users, through: users_groups
 end
