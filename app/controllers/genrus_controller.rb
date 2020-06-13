439952c9c5245e58214b4bb1a7e2129dd7f9513e
niisan3211 <kappahaookikunaritai@icloud.com>
2020-06-13 15:11:00 +0900

    add:genrus_controller

--- app/controllers/genrus_controller.rb
+++ app/controllers/genrus_controller.rb
@@ -1,4 +1,24 @@
 class GenrusController < ApplicationController
   def index
   end
+
+  def new
+    @genru = Genru.new
+    @genrus = Genru.all.order("name")
+    # あいうえお順、アルファベット順になる
+    @genru_search = @genrus.where(['name LIKE ?', "%#{params[:search]}%"]) if params[:search].present?
+    # redirect_to action: "new"
+  end
+
+  def create
+    @genru = Genru.new(genru_params)
+    @genru.save
+    redirect_to  action: "new"
+  end
+
+  private
+
+  def genru_params
+    params.require(:genru).permit(:name)
+  end
 end
