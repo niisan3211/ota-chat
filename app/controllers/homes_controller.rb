404ca4e7f47383351ad0b5e3faf7ce038e88feb7
niisan3211 <kappahaookikunaritai@icloud.com>
2020-08-31 01:23:43 +0900

    変更
    必要ないコードを発見したため

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,5 @@
 class HomesController < ApplicationController
   def index
-    @genrus = Genru.where(user_id: current_user.id)
     @user = User.find(current_user.id)
     @groups = @user.users_groups
   end
